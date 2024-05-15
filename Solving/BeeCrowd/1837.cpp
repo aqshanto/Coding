5bfe3e7b49768908459222a67cd602597db57a4b
aqShanto <abdulquymshanto@gmail.com>
2024-05-15 22:41:45 +0600

    update

--- Solving/BeeCrowd/1837.cpp
+++ Solving/BeeCrowd/1837.cpp
@@ -3,12 +3,40 @@
 int main(){
     int a,b,q,r;
     scanf("%d %d",&a,&b);
-    if((a%b)<0){
-        q=(a/b)-1;
-        r=a-(b*q);
-    }else{
-        q=a/b;
-        r=a%b;
+    if(a==0 && b==0){
+        q=0;
+        r=0;
+    }
+    else if(a>=0){
+        if(b==0){
+            q=0;
+            r=0;
+        }else{
+            q=a/b;
+            r=a%b;
+        }
+    }else if(a<0){
+        if(b==0){
+            q=0;
+            r=0;
+        }
+        else if(b>0){
+            for(int i=0; ;i--){
+                if(a>=b*i){
+                    q=i;
+                    r=a-b*i;
+                    break;
+                }
+            }
+        }else if(b<0){
+            for(int i=0; ;i++){
+                if(a>=b*i){
+                    q=i;
+                    r=a-b*i;
+                    break;
+                }
+            }
+        }
     }
     printf("%d %d\n",q,r);
     return 0;
