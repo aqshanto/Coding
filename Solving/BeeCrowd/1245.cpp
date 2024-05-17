41c4302431ca49bc82674413455ab47281aa49e6
aqShanto <abdulquymshanto@gmail.com>
2024-05-18 02:08:43 +0600

    up

--- Solving/BeeCrowd/1245.cpp
+++ Solving/BeeCrowd/1245.cpp
@@ -3,30 +3,18 @@
 using namespace std;
 
 int main(){
-    while(!cin.eof()){
-        int test,num,total=0;
-        char ch;
-        cin>>test;
-        int E[61]={0};
-        int D[61]={0};
+    int test;
+    while(cin>>test){
+        int T[100][2];
+        memset(T,0,sizeof T);
         for(int i=0;i<test;i++){
+            int num;char ch;
             cin>>num>>ch;
-            if(ch=='E'){
-                E[num]++;
-            }else if(ch=='D'){
-                D[num]++;
-            }
+            T[num][ch=='D']++;
         }
-        for(int i=30;i<=60;i++){
-            if(E[i]>0 && D[i]>0){
-                if(E[i]==D[i]){
-                    total+=E[i];
-                }else if(E[i]<D[i]){
-                    total+=E[i];
-                }else if(E[i]>D[i]){
-                    total+=D[i];
-                }
-            }
+        int total=0;
+        for(int i=0;i<100;i++){
+            total+=min(T[i][0],T[i][1]);
         }
         cout<<total<<endl;
     }
