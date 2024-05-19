d3c748e47c2af5bb9b608933c18b51c73f13ceb9
aqShanto <abdulquymshanto@gmail.com>
2024-05-19 23:36:50 +0600

    up

---
+++ CPP_Programming/Phitron/switchcase.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    switch (n){
+    case 1:
+        cout<<"saturday"<<endl;
+        break;
+    case 2 :
+        cout<<"Sunday"<<endl;
+        break;
+    case 3 :
+        cout<<"Monday"<<endl;
+        break;
+    case 4 :
+        cout<<"Tuesday"<<endl;
+        break;
+    case 5 :
+        cout<<"Wednesday"<<endl;
+        break;
+    case 6 :
+        cout<<"Thursday"<<endl;
+        break;
+    case 7 :
+        cout<<"Friday"<<endl;
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
