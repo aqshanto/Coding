130380f4f529043f865f0e94170aeab4aa1ad676
aqShanto <abdulquymshanto@gmail.com>
2024-05-23 00:50:23 +0600

    up

---
+++ Solving/Hackerearth/Contest 1/City_tour.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        long long int n;
+        cin>>n;
+        cout<<n*4<<endl;
+    }
+    return 0;
+}
