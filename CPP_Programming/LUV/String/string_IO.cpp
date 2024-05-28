7ef54cb7e692b426904834570342c99fd3810415
aqShanto <abdulquymshanto@gmail.com>
2024-05-28 13:45:55 +0600

    up

---
+++ CPP_Programming/LUV/String/string_IO.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //String Input output
+
+    // string str="Hello";
+    // cout<<str<<endl;
+    // string str1;
+    // cin>>str1;
+    // cout<<str1<<endl;
+
+    //string concetenation
+
+    string str1="Hello";
+    string str2="World";
+    cout<<str1+str2<<endl;
+    cout<<str1+" "+str2<<endl;
+
+    
+    return 0;
+}
