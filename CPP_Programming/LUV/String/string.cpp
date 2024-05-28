45e092e91ac5dba3ab6de128fa5dca065997cc93
aqShanto <abdulquymshanto@gmail.com>
2024-05-28 14:01:50 +0600

    up

--- CPP_Programming/LUV/String/string.cpp
+++ CPP_Programming/LUV/String/string.cpp
@@ -3,20 +3,50 @@
 using namespace std;
 
 int main(){
-    //String Input output
+    // //String Input output
 
     // string str="Hello";
     // cout<<str<<endl;
-    // string str1;
-    // cin>>str1;
-    // cout<<str1<<endl;
+    // string str0,stro;
+    // cin>>str0>>stro;
+    // cout<<str0<<" "<<stro<<endl;
 
-    //string concetenation
+    // //string concetenation
 
-    string str1="Hello";
-    string str2="World";
-    cout<<str1+str2<<endl;
-    cout<<str1+" "+str2<<endl;
+    // string str1="Hello";
+    // string str2="World";
+    // cout<<str1+str2<<endl;
+    // cout<<str1+" "+str2<<endl;
+
+    // //String Equat
+
+    // if(str1==str2){
+    //     cout<<"Equal"<<endl;
+    // }else{
+    //     cout<<"Not equal"<<endl;
+    // }
+
+    // //string indexing
+
+    // string ch= "shanto";
+    // cout<<ch[3]<<endl;
+    // ch[0]='a';
+    // cout<<ch<<endl;
+
+    // int string_size= ch.size();
+    // for(int i=0;i<string_size;i++){
+    //     cout<<ch[i]<<endl;
+    // }
+
+    //getline function
+
+    string str1,str2;
+    cin>>str1>>str2;
+    cout<<str1<<" "<<str2<<endl;
+    string str3;
+    cin.ignore();
+    getline(cin,str3);
+    cout<<str3<<endl;
 
     
     return 0;
