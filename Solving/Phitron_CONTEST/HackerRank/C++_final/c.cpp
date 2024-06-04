3ec42197a1ce335b52a977da8d1255de2a47dac2
aqShanto <abdulquymshanto@gmail.com>
2024-06-04 20:21:39 +0600

    up

--- Solving/Phitron_CONTEST/HackerRank/C++_final/c.cpp
+++ Solving/Phitron_CONTEST/HackerRank/C++_final/c.cpp
@@ -1,33 +1,32 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-class student
-{
+class student{
 public:
     string nm, s;
     int cls, id;
 };
-int main()
-{
+
+int main(){
     int N;
+
     cin >> N;
+
     student st[N];
-    for (int i = 0; i < N; i++)
-    {
-        cin >> st[i].nm >> st[i].cls >> st[i].s >> st[i].id;
+
+    for (int j=0;j<N;j++){
+        cin >> st[j].nm >> st[j].cls >> st[j].s >> st[j].id;
     }
     string ar[N];
-    for (int i = 0; i < N; i++)
-    {
+    for (int i=0;i<N;i++){
         ar[i] = st[i].s;
     }
-    for (int i = 0; i < N; i++)
-    {
-        st[i].s = ar[N - i - 1];
+    for (int k=0;k<N;k++){
+        st[k].s = ar[N-k-1];
     }
-    for (int i = 0; i < N; i++)
-    {
-        cout << st[i].nm << " " << st[i].cls << " " << st[i].s << " " << st[i].id << endl;
+    for (int h=0;h<N;h++){
+        cout << st[h].nm << " " << st[h].cls << " " << st[h].s << " " << st[h].id << endl;
     }
     return 0;
 }
