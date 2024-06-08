43f1d17bbdae45865a902941a56eaeef3e21ba23
aqShanto <abdulquymshanto@gmail.com>
2024-06-09 01:59:36 +0600

    up

---
+++ Solving/CodeForce/cf1030a.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,sum=0,num;
+    cin>>n;
+    while(n--){
+        num;
+        cin>>num;
+        sum+=num;
+    }
+    if(sum==0) cout<<"EASY"<<endl;
+    else cout<<"HARD"<<endl;
+    return 0;
+}
