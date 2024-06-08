e5b94f56b3bf3169490ae0ce708aa9d62e1f9ded
aqShanto <abdulquymshanto@gmail.com>
2024-06-09 02:18:00 +0600

    up

---
+++ Solving/TakeOFF/Take_off_contest_2023/b.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x,a,b,c;
+    cin>>x>>a>>b>>c;
+    if(a<=x && b<=x && c<=x){
+        cout<<"OK"<<endl;
+    }else{
+        cout<<"ERROR"<<endl;
+    }
+    return 0;
+}
---
+++ Solving/TakeOFF/Take_off_contest_2023/c.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,k,x;
+    cin>>n>>k>>x;
+    cout<<((n-k)+1)*x*2<<endl;
+    return 0;
+}
