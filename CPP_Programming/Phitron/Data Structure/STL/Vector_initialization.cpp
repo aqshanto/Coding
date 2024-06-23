c9029831161461065a2ea9caf151abdc568b5895
aqShanto <abdulquymshanto@gmail.com>
2024-06-23 21:36:03 +0600

    up

---
+++ CPP_Programming/Phitron/Data Structure/STL/Vector_initialization.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    // vector <int> v;          //type 1
+    // cout<<v.size()<<endl;
+
+    // vector <int> v(5);       //type 2
+    // cout<<v.size()<<endl;
+
+    // vector <int> v(5,10);    //type 3
+    // for(int i=0;i<5;i++){
+    //     cout<<v[i]<<' ';
+    // }
+    // cout<< v2.size()<<endl;
+
+    // vector <int> v(5,100);   //type 4
+    // vector <int> v2(v);
+    // for(int i=0;i<5;i++){
+    //     cout<<v2[i]<<' ';
+    // }cout<<endl;
+    // cout<< v2.size()<<endl;
+
+    // int a[5]={7,8,34,18,54}; //type 5
+    // vector <int> v(a,a+5);
+    // for(int i=0;i<v.size();i++){
+    //     cout<<v[i]<<' ';
+    // }cout<<endl;
+    // cout<< v.size()<<endl;
+
+    return 0;
+}
